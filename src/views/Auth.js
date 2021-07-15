afcee93a96f7b330ba08d4873058700e7ed6b5da
Shane Vadnais <vadna034@umn.edu>
2021-07-15 15:38:06 -0500

    Added firebase login ui to Auth page

--- src/views/Auth.js
+++ src/views/Auth.js
@@ -1,3 +1,69 @@
+import React, { useContext, useEffect } from 'react';
+import firebase from 'firebase';
+
+import { FirebaseAuth } from 'react-firebaseui';
+import { AuthContext } from '../context/AuthContext';
+
+import 'spectre.css';
+
 export default function Auth() {
-  return <h1>Auth</h1>;
+  //get the user state from the context
+  const { user, setUser } = useContext(AuthContext);
+
+  const pfp = {
+    'border-radius': '100%',
+    margin: '1em 1em 0.5em 1em',
+  };
+
+  //this is our config for FirebaseAuth
+  const uiConfig = {
+    signInFlow: 'redirect',
+    signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
+    callbacks: {
+      signInSuccess: () => {
+        return false;
+      },
+    },
+  };
+
+  //if user exists or signed in, we redirect the page to home, else display the sign in methods with FirebaseAuth
+  return (
+    <div>
+      {!!user ? (
+        <div
+          class="profile-logout-div"
+          style={{
+            width: '75%',
+            margin: 'auto',
+          }}
+        >
+          <img src={user.photoURL} alt="Null" style={pfp} />
+          <p>
+            {' '}
+            <strong>User:</strong> {user.displayName}
+          </p>
+          <button
+            class="btn"
+            style={{ margin: '0em 0em 1em' }}
+            onClick={() => {
+              firebase
+                .auth()
+                .signOut()
+                .then(() => {
+                  setUser(null);
+                });
+            }}
+          >
+            {' '}
+            sign out
+          </button>
+        </div>
+      ) : (
+        <div>
+          <p>Please Sign In</p>
+          <FirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+        </div>
+      )}
+    </div>
+  );
 }
