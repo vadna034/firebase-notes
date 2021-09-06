3ce8d99ceca3410766ea6d6d4c2c452cf132f4d6
Shane Vadnais <vadna034@umn.edu>
2021-09-05 22:25:31 -0500

    cleaned up Auth

--- src/views/Auth.js
+++ src/views/Auth.js
@@ -1,30 +1,25 @@
-import React, { useContext, useEffect } from 'react';
+import React from 'react';
 import firebase from 'firebase';
 
 import { FirebaseAuth } from 'react-firebaseui';
-import { AuthContext } from '../context/AuthContext';
-
-import 'spectre.css';
 
 export default function Auth() {
-  //get the user state from the context
-  //this is our config for FirebaseAuth
-  const uiConfig = {
-    signInFlow: 'redirect',
-    signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
-    callbacks: {
-      signInSuccess: () => {
-        return false;
-      },
-    },
-  };
-
-  //if user exists or signed in, we redirect the page to home, else display the sign in methods with FirebaseAuth
   return (
     <div>
       <div>
         <p>Please Sign In</p>
-        <FirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+        <FirebaseAuth
+          uiConfig={{
+            signInFlow: 'redirect',
+            signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
+            callbacks: {
+              signInSuccess: () => {
+                return false;
+              },
+            },
+          }}
+          firebaseAuth={firebase.auth()}
+        />
       </div>
     </div>
   );
