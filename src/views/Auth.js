d27be0f6cb67fe2521f5c1b6c8918203dad0e22f
Shane Vadnais <shanevadnais@outlook.com>
2025-01-31 18:13:13 -0600

    Fixed login form

--- src/views/Auth.js
+++ src/views/Auth.js
@@ -10,7 +10,7 @@ export default function Auth() {
         <p>Please Sign In</p>
         <FirebaseAuth
           uiConfig={{
-            signInFlow: 'redirect',
+            signInFlow: 'popup',
             signInOptions: [providerID],
             callbacks: {
               signInSuccess: () => {
